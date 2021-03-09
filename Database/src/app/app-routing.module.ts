8f45f622457d8eb1c5c36dcaa9e9bf5ccbf48914
MuhammadIzzul <80318655+MuhammadIzzul@users.noreply.github.com>
2021-03-09 17:55:40 +0800

    Mobile App Project

---
+++ Database/src/app/app-routing.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'homeadmin',
+    loadChildren: () => import('./homeadmin/homeadmin.module').then( m => m.HomeadminPageModule)
+  },
+  {
+    path: 'assets',
+    loadChildren: () => import('./assets/assets.module').then( m => m.AssetsPageModule)
+  },
+  {
+    path: 'activity',
+    loadChildren: () => import('./activity/activity.module').then( m => m.ActivityPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+  },
+  {
+    path: 'view-post',
+    loadChildren: () => import('./view-post/view-post.module').then( m => m.ViewPostPageModule)
+  },
+  {
+    path: 'edit-post/:id',
+    loadChildren: () => import('./edit-post/edit-post.module').then( m => m.EditPostPageModule)
+  },
+  {
+    path: 'faq',
+    loadChildren: () => import('./faq/faq.module').then( m => m.FAQPageModule)
+  },
+
+  
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Database/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+ production: false,
+ firebaseConfig: {
+ apiKey: "AIzaSyAuTThF8mIkLImEquk4d-7W3uwZ3wAh8Zo",
+ authDomain: "database-cb731.firebaseapp.com",
+ databaseURL: "https://database-cb731.firebaseio.com",
+ projectId: "database-cb731",
+ storageBucket: "database-cb731.appspot.com",
+ messagingSenderId: "823757861549",
+ appId: "1:823757861549:web:6374aa64be92eb4f934c24",
+ measurementId: "G-LEGFCZRCF9"
+ }
+};
